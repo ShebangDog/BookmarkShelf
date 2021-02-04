72f893f164e476fc1e194aea9ae36baee1fd051c
ShebangDog <e1n18088@oit.ac.jp>
2021-02-04 17:27:45 +0900

    shelf画面を追加する

--- feature/shelf/src/main/java/dog/shebang/shelf/ShelfFragment.kt
+++ feature/shelf/src/main/java/dog/shebang/shelf/ShelfFragment.kt
@@ -1,7 +1,14 @@
 package dog.shebang.shelf
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.wada811.viewbinding.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class ShelfFragment : Fragment() {
+@AndroidEntryPoint
+class ShelfFragment : Fragment(R.layout.fragment_shelf) {
+
+    private val binding: ShelfFragment by viewBinding()
+    private val viewModel: ShelfViewModel by viewModels()
 
 }
