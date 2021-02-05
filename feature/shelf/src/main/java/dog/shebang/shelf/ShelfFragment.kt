df2abcab04846fd1df4dd42e3bf62e69991ba7e3
ShebangDog <e1n18088@oit.ac.jp>
2021-02-05 15:17:57 +0900

    recyclerviewでbookmarkを一覧表示する

--- feature/shelf/src/main/java/dog/shebang/shelf/ShelfFragment.kt
+++ feature/shelf/src/main/java/dog/shebang/shelf/ShelfFragment.kt
@@ -1,14 +1,49 @@
 package dog.shebang.shelf
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.wada811.viewbinding.viewBinding
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.viewbinding.GroupieViewHolder
 import dagger.hilt.android.AndroidEntryPoint
+import dog.shebang.component.databinding.LayoutBookmarkCardBinding
+import dog.shebang.component.ext.setSpan
+import dog.shebang.shelf.databinding.FragmentShelfBinding
+import dog.shebang.shelf.item.DefaultPreviewItem
 
 @AndroidEntryPoint
 class ShelfFragment : Fragment(R.layout.fragment_shelf) {
 
-    private val binding: ShelfFragment by viewBinding()
+    private val binding: FragmentShelfBinding by viewBinding()
     private val viewModel: ShelfViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = GroupAdapter<GroupieViewHolder<LayoutBookmarkCardBinding>>()
+
+        binding.apply {
+            shelfRecyclerView.setSpan(all = 32)
+
+            shelfRecyclerView.adapter = adapter
+        }
+
+        viewModel.bookmarkListLiveData.observe(viewLifecycleOwner) { bookmarkList ->
+            val bookmarkItemList = bookmarkList.map {
+                DefaultPreviewItem(it) { _, url -> browse(url) }
+            }
+
+            adapter.update(bookmarkItemList)
+        }
+    }
+
+    private fun browse(url: String) {
+        val browseIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+
+        startActivity(browseIntent)
+    }
 }
