7efad86bd339aebfe6f603fe9598fbe0125eee7f
ShebangDog <e1n18088@oit.ac.jp>
2021-02-21 00:31:37 +0900

    LinkPreviewApiのエラーを処理できるようにする

--- data/src/main/java/dog/shebang/data/api/LinkPreviewApi.kt
+++ data/src/main/java/dog/shebang/data/api/LinkPreviewApi.kt
@@ -1,6 +1,7 @@
 package dog.shebang.data.api
 
 import dog.shebang.data.api.entity.MetadataEntity
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -9,5 +10,5 @@ interface LinkPreviewApi {
     suspend fun fetchMetadata(
         @Query("key") key: String,
         @Query("q") url: String
-    ): MetadataEntity
+    ): Response<MetadataEntity>
 }
