d875f27cbc77b05ee2fac8c02c339d3898ad41dd
ShebangDog <e1n18088@oit.ac.jp>
2021-02-24 02:12:57 +0900

    境界を設定する

--- model/src/main/java/dog/shebang/model/Result.kt
+++ model/src/main/java/dog/shebang/model/Result.kt
@@ -1,6 +1,6 @@
 package dog.shebang.model
 
-sealed class Result<T> {
+sealed class Result<out T> {
     data class Success<T>(val value: T) : Result<T>()
     data class Failure<Nothing>(val throwable: Throwable) : Result<Nothing>()
 }
