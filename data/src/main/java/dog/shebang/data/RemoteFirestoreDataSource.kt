0458593e2115f664bb65146ee0206bbb4ccbe842
ShebangDog <e1n18088@oit.ac.jp>
2021-02-24 22:20:44 +0900

    LoadStateで読み込み状態を取り扱うようにする

--- data/src/main/java/dog/shebang/data/RemoteFirestoreDataSource.kt
+++ data/src/main/java/dog/shebang/data/RemoteFirestoreDataSource.kt
@@ -2,12 +2,17 @@ package dog.shebang.data
 
 import dog.shebang.data.firestore.BookmarkFirestore
 import dog.shebang.model.Bookmark
+import dog.shebang.model.LoadState
+import dog.shebang.model.Result
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 interface RemoteFirestoreDataSource {
 
-    fun fetchBookmarkList(): Flow<List<Bookmark>>
+    fun fetchBookmarkList(): Flow<LoadState<List<Bookmark>>>
 
     suspend fun storeBookmark(bookmark: Bookmark)
 }
@@ -16,7 +21,18 @@ class RemoteFirestoreDataSourceImpl @Inject constructor(
     private val bookmarkFirestore: BookmarkFirestore
 ) : RemoteFirestoreDataSource {
 
-    override fun fetchBookmarkList(): Flow<List<Bookmark>> = bookmarkFirestore.fetchBookmarkList()
+    override fun fetchBookmarkList(): Flow<LoadState<List<Bookmark>>> = flow {
+        emit(LoadState.Loading)
+
+        val loadStateFlow = bookmarkFirestore.fetchBookmarkList().map { result ->
+            when (result) {
+                is Result.Failure -> LoadState.Error(result.throwable)
+                is Result.Success -> LoadState.Loaded(result.value)
+            }
+        }
+
+        emitAll(loadStateFlow)
+    }
 
     override suspend fun storeBookmark(bookmark: Bookmark) {
         bookmarkFirestore.storeBookmark(bookmark)
