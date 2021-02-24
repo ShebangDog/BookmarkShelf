1362a2d0a78d83b95848051a8fcaf16f9d8dc929
ShebangDog <e1n18088@oit.ac.jp>
2021-02-24 21:29:35 +0900

    Resultでエラーを取り扱うようにする

--- data/src/main/java/dog/shebang/data/firestore/BookmarkFirestore.kt
+++ data/src/main/java/dog/shebang/data/firestore/BookmarkFirestore.kt
@@ -6,18 +6,18 @@ import com.google.firebase.ktx.Firebase
 import dog.shebang.data.firestore.entity.BookmarkEntity
 import dog.shebang.data.firestore.ext.bookmarksRef
 import dog.shebang.model.Bookmark
+import dog.shebang.model.Result
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
-import java.util.*
 import javax.inject.Inject
 
 interface BookmarkFirestore {
 
-    fun fetchBookmarkList(): Flow<List<Bookmark>>
+    fun fetchBookmarkList(): Flow<Result<List<Bookmark>>>
 
     suspend fun storeBookmark(bookmark: Bookmark)
 }
@@ -27,20 +27,31 @@ class BookmarkFirestoreImpl @Inject constructor() : BookmarkFirestore {
     private val firestore = Firebase.firestore
 
     @ExperimentalCoroutinesApi
-    override fun fetchBookmarkList(): Flow<List<Bookmark>> = callbackFlow {
-        firestore.bookmarksRef(Firebase.auth.currentUser?.uid ?: "null")
-            .addSnapshotListener { snapshot, exception ->
-                exception?.run { return@run }
+    override fun fetchBookmarkList(): Flow<Result<List<Bookmark>>> = callbackFlow {
+        val listenerRegistration = try {
+            val uid = Firebase.auth.currentUser?.uid ?: throw FirebaseNotLoggedException
 
-                val entityList = snapshot?.toObjects(BookmarkEntity::class.java)
+            firestore.bookmarksRef(uid)
+                .addSnapshotListener { snapshot, exception ->
+                    exception?.run { throw this }
 
-                val bookmarkList = entityList
-                    ?.mapNotNull { it.modelOrNull() }
-                    ?: emptyList()
+                    val entityList = snapshot?.toObjects(BookmarkEntity::class.java)
 
-                offer(bookmarkList)
+                    val bookmarkList = entityList
+                        ?.mapNotNull { it.modelOrNull() }
+                        ?: emptyList()
 
-            }.also { awaitClose { it.remove() } }
+                    offer(Result.Success(bookmarkList))
+
+                }
+
+        } catch (throwable: Throwable) {
+
+            offer(Result.Failure<Nothing>(throwable))
+            null
+        }
+
+        awaitClose { listenerRegistration?.remove() }
     }
 
     @ExperimentalCoroutinesApi
