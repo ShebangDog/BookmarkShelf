1d78de67a275381f3e835919e796451e656bb462
ShebangDog <e1n18088@oit.ac.jp>
2021-03-16 10:32:38 +0900

    すべてのカテゴリを表すカテゴリAllを追加する

--- feature/category/src/main/java/dog/shebang/category/MainViewModel.kt
+++ feature/category/src/main/java/dog/shebang/category/MainViewModel.kt
@@ -1,9 +1,12 @@
 package dog.shebang.category
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dog.shebang.data.datasource.remote.RemoteCategoryDataSource
+import dog.shebang.model.Category
 import dog.shebang.model.LoadState
 import kotlinx.coroutines.flow.collect
 import javax.inject.Inject
@@ -13,9 +16,16 @@ class MainViewModel @Inject constructor(
     remoteCategoryDataSource: RemoteCategoryDataSource
 ) : ViewModel() {
 
+    private val isMutableSelectedCategoryLiveData = MutableLiveData(Category.defaultCategory)
+    val selectedCategoryLiveData: LiveData<Category> = isMutableSelectedCategoryLiveData
+
     val categoryListLiveData = liveData {
         remoteCategoryDataSource.fetchCategoryList().collect { loadState ->
             if (loadState is LoadState.Loaded) emit(loadState.value)
         }
+    }
+
+    fun onCategorySelected(category: Category) {
+        isMutableSelectedCategoryLiveData.value = category
     }
 }
