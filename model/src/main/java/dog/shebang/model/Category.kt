ef473997669a23e46df975a2fe1f9c9457760c0c
ShebangDog <e1n18088@oit.ac.jp>
2021-03-21 10:28:58 +0900

    colorのequals, hashcodeを実装する

--- model/src/main/java/dog/shebang/model/Category.kt
+++ model/src/main/java/dog/shebang/model/Category.kt
@@ -53,4 +53,21 @@ class Color private constructor(internalColor: android.graphics.Color) {
             return valueOf(randomElem(), randomElem(), randomElem())
         }
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Color
+
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return value
+    }
+
+
 }
