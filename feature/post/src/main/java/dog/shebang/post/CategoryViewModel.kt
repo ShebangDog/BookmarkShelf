b551b44001b80ef0d79135b709e7c0308d0b1dc5
ShebangDog <e1n18088@oit.ac.jp>
2021-04-02 17:53:16 +0900

    CategoryViewModelでAuthViewModelDelegateを実装する

--- feature/post/src/main/java/dog/shebang/post/CategoryViewModel.kt
+++ feature/post/src/main/java/dog/shebang/post/CategoryViewModel.kt
@@ -2,15 +2,37 @@ package dog.shebang.post
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dog.shebang.core.AuthViewModelDelegate
 import dog.shebang.data.repository.CategoryRepository
+import dog.shebang.model.Category
+import dog.shebang.model.LoadState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CategoryViewModel @Inject constructor(
-    categoryRepository: CategoryRepository
-) : ViewModel() {
+    private val categoryRepository: CategoryRepository,
+    authViewModelDelegate: AuthViewModelDelegate
+) : ViewModel(), AuthViewModelDelegate by authViewModelDelegate {
 
-    val categoryListLiveData = categoryRepository.fetchCategoryList().asLiveData()
+    val currentFirebaseUserState = firebaseUserInfoFlow
+        .stateIn(viewModelScope, SharingStarted.Eagerly, null)
+
+    private val mutableCategoryListStateFlow =
+        MutableStateFlow<LoadState<List<Category>>>(LoadState.Loading)
+
+    fun updateUserData(uid: String?) = viewModelScope.launch {
+        mutableCategoryListStateFlow.emitAll(
+            categoryRepository.fetchCategoryList(uid)
+        )
+    }
+
+    val categoryListLiveData = mutableCategoryListStateFlow.asLiveData()
 
 }
