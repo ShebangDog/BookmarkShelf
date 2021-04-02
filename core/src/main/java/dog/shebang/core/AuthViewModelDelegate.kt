79f3535f1af0c2dc3352d0fb42f016ff47b12a17
ShebangDog <e1n18088@oit.ac.jp>
2021-04-02 18:25:45 +0900

    認証情報をUserInfoに詰める

--- core/src/main/java/dog/shebang/core/AuthViewModelDelegate.kt
+++ core/src/main/java/dog/shebang/core/AuthViewModelDelegate.kt
@@ -1,13 +1,13 @@
 package dog.shebang.core
 
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dog.shebang.model.UserInfo
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
@@ -15,7 +15,7 @@ import kotlinx.coroutines.flow.callbackFlow
 import javax.inject.Singleton
 
 interface AuthViewModelDelegate {
-    val firebaseUserInfoFlow: Flow<FirebaseUser?>
+    val userInfoFlow: Flow<UserInfo?>
 }
 
 class DefaultAuthViewModelDelegate : AuthViewModelDelegate {
@@ -23,9 +23,18 @@ class DefaultAuthViewModelDelegate : AuthViewModelDelegate {
     private val firebaseAuth = Firebase.auth
 
     @ExperimentalCoroutinesApi
-    override val firebaseUserInfoFlow: Flow<FirebaseUser?> = callbackFlow {
+    override val userInfoFlow: Flow<UserInfo?> = callbackFlow {
         val authStateListener: (FirebaseAuth) -> Unit = {
-            offer(it.currentUser)
+            try {
+                val userInfo = UserInfo(
+                    uid = it.currentUser?.uid!!,
+                    profileIconUrl = it.currentUser?.photoUrl?.path
+                )
+
+                offer(userInfo)
+            } catch (throwable: Throwable) {
+                offer(null)
+            }
         }
 
         firebaseAuth.addAuthStateListener(authStateListener)
